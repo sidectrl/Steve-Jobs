d7acb866ac215522a3defa867d058ea66d4bfbae
Sidectrl <limonellifrancesco@gmail.com>
2022-05-06 22:45:12 +0200

    sistemazione struttura algoritmo, per la prossime modifiche bisogna levare i warning che rompono u cazz

--- PHP/Esercizi/Es2/index.php
+++ PHP/Esercizi/Es2/index.php
@@ -1,6 +1,6 @@
 <form method="GET" action="index.php">
     <label for="input">Valori vettore</label>
-    <input type="text" name="vettore" value="5, 6, 7, 8, 9, 10, 5, 20, 7, 8" />
+    <input type="text" name="vettore" />
     <input type="submit" value="Inserimento valori" />
     <select name="operazione" id="">
         <option value="Somma">Somma</option>
@@ -11,7 +11,7 @@
         <option value="Search">Trova un valore</option>
         <option value="Remove">Rimuovi un valore</option>
         <option value="CheckCrescente">Controlla la crescenza</option>
-    </select>
+    </select><br>
     <input type="text" name="search_or_remove"><br>
     <?php
     if (isset($_GET["vettore"])) {
@@ -19,84 +19,38 @@
         $array = explode(', ', $dummy);
         ShowVector($array);
     }
-    if ((isset($_GET["operazione"]))) {
+    if ((isset($_GET["operazione"])) and (isset($_GET["vettore"]))) {
         switch ($_GET['operazione']) {
             case "Somma":
-                $sum = 0;
-                foreach ($array as $value) {
-                    $sum = $sum + $value;
-                }
+                $sum = Sum($array);
                 echo "La somma di tutti i componenti del vettore è: " . $sum;
                 break;
             case "Max_Value":
-                $max = 0;
-                for ($i = 0; $i < sizeof($array); $i++)
-                    if (isset($array[$i])) {
-                        $max = $array[$i];
-                        break;
-                    }
-                foreach ($array as $value) {
-                    if ($max < $value)
-                        $max = $value;
-                }
+                $max = Max_Value($array);
                 echo "Il valore massimo presente nel vettore è: " . $max;
                 break;
             case "Min_Value":
-                $min = $array[0];
-                for ($i = 0; $i < sizeof($array); $i++)
-                    if (isset($array[$i])) {
-                        $max = $array[$i];
-                        break;
-                    }
-                foreach ($array as $value) {
-                    if ($min > $value)
-                        $min = $value;
-                }
+                $min = Min_Value($array);
                 echo "Il valore minimo presente nel vettore è: " . $min;
                 break;
             case "Med_Value":
-                $media = 0;
-                foreach ($array as $value) {
-                    $media += $value;
-                }
-                $media = $media / count($array);
-                echo "La media di tutti i valori è:" . round($media, 2);
+                $media = Media_value($array);
+                echo "La media di tutti i valori è:" . $media;
                 break;
             case "Pari_dispari":
-                $count_pari = 0;
-                $count_dispari = 0;
-                foreach ($array as $value) {
-                    if ($value % 2 == 0)
-                        $count_pari++;
-                    else
-                        $count_dispari++;
-                }
-                echo "I numeri pari sono: " . $count_pari . ", i numeri dispari sono: " . $count_dispari;
+                $pari_dispari = Pari_Dispari($array);
+                echo "I numeri pari sono: " . $pari_dispari[0] . ", i numeri dispari sono: " . $pari_dispari[1];
                 break;
             case "Search":
                 if (isset($_GET["search_or_remove"])) {
-                    $arr_pos = 0;
-                    $value = $_GET["search_or_remove"];
-                    echo "Il valore si trova nelle celle: ";
-                    for ($i = 0; $i < sizeof($array); $i++) {
-                        if ($array[$i] == $value) {
-                            $position[$arr_pos] = $i;
-                            echo  $position[$arr_pos] . " ";
-                            $arr_pos++;
-                        }
-                    }
+                    Trova($array);
                 } else
                     echo "Valore da cercare non inserito correttamente";
                 break;
             case "Remove":
                 if (isset($_GET["search_or_remove"])) {
-                    $array_leght = sizeof($array);
-                    for ($i = 0; $i < $array_leght; $i++) {
-                        if ($array[$i] == $value) {
-                            unset($array[$i]);
-                        }
-                    }
-                    ShowVector($vettore);
+                    $array = Remove($array);
+                    ShowVector($array);
                 } else
                     echo "Valore da rimuovere non inserito correttamente";
                 break;
@@ -111,12 +65,83 @@
                 break;
         }
     }
+    function Sum($vettore)
+    {
+        $sum = 0;
+        foreach ($vettore as $value) {
+            $sum = $sum + $value;
+        }
+        return $sum;
+    }
+    function Max_Value($vettore)
+    {
+        $max = 0;
+        for ($i = 0; $i < sizeof($vettore); $i++)
+            if (isset($vettore[$i])) {
+                $max = $vettore[$i];
+                break;
+            }
+        foreach ($vettore as $value) {
+            if ($max < $value)
+                $max = $value;
+        }
+        return $max;
+    }
+    function Min_Value($vettore)
+    {
+        $min = $vettore[0];
+        for ($i = 0; $i < sizeof($vettore); $i++)
+            if (isset($vettore[$i])) {
+                $max = $vettore[$i];
+                break;
+            }
+        foreach ($vettore as $value) {
+            if ($min > $value)
+                $min = $value;
+        }
+        return $min;
+    }
+    function Media_value($vettore)
+    {
+        $media = 0;
+        foreach ($vettore as $value) {
+            $media += $value;
+        }
+        $media = $media / count($vettore);
+        return round($media, 2);
+    }
+    function Pari_Dispari($vettore)
+    {
+        $count_pari = 0;
+        $count_dispari = 0;
+        foreach ($vettore as $value) {
+            if ($value % 2 == 0)
+                $count_pari++;
+            else
+                $count_dispari++;
+        }
+        return array($count_pari, $count_dispari);
+    }
+    function Trova($vettore)
+    {
+        $arr_pos = 0;
+        $value = $_GET["search_or_remove"];
+        echo "Il valore si trova nelle celle: ";
+        for ($i = 0; $i < sizeof($vettore); $i++) {
+            if ($vettore[$i] == $value) {
+                $position[$arr_pos] = $i;
+                echo  $position[$arr_pos] . " ";
+                $arr_pos++;
+            }
+        }
+    }
     function ShowVector($arrayFunc)
     {
         for ($i = 0; $i < sizeof($arrayFunc); $i++) {
             echo "array[" . $i . "]= " . $arrayFunc[$i] . "<br>";
         }
     }
+
     function CheckCrescente($vettore)
     {
         $dummy = 0;
@@ -132,6 +157,26 @@
                 return false;
 
         return true;
+    }
+    function Remove($vettore)
+    {
+        $array_leght = sizeof($vettore);
+        $value = $_GET["search_or_remove"];
+        for ($i = 0; $i < $array_leght; $i++) {
+            if ($vettore[$i] == $value) {
+                unset($vettore[$i]);
+            }
+        }
+        $count2 = 0;
+        for ($i = 0; $i < $array_leght; $i++) {
+            if (is_null($vettore[$count2])) {
+                $count2++;
+                $vettore2[$i] = $vettore[$count2];
+            } else
+                $vettore2[$i] = $vettore[$count2];
+            $count2++;
+        }
+        return $vettore2;
     }
     ?>
 </form>
