e02005eb89eff397d39f83efec88c465e237e667
Sidectrl <limonellifrancesco@gmail.com>
2022-05-24 09:18:08 +0200

    primo esercizio java

--- Java/MyClass.java
+++ Java/MyClass.java
@@ -1,6 +1,8 @@
 package Java;
 public class MyClass {
     public static void main(String[] args) {
-        
+        circle mycircle = new circle(2.5);
+        System.out.println("Il perimetro è: " + mycircle.GetPerimeter());
+        System.out.println("L'area è: " + mycircle.GetArea());
     }
 };
---
+++ Java/circle.java
@@ -0,0 +1,31 @@
+package Java;
+public class circle {
+    //#region attributes
+    private double radius = 0;
+    //#endregion
+
+    //#region costructor
+    public circle(double value)
+    {
+        this.radius = value;
+    }
+    //#endregion
+    //#region Methods
+    public void SetRadius(double value)
+    {
+        this.radius = value;
+    }
+
+    public double GetPerimeter(){
+        double perimeter = 0;
+        perimeter = (3.14*2)*radius;
+        return perimeter;
+    }
+    public double GetArea()
+    {
+        double area = 0;
+        area = (radius*radius)*3.14;
+        return area;
+    }
+    //#endregion
+}
