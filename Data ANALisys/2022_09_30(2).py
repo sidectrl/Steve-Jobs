0769bf1b8df3d5ee3c358e45af76b73917eb4175
Sidectrl <limonellifrancesco@gmail.com>
2022-09-30 11:45:13 +0200

    es

--- Data ANALisys/2022_09_30(2).py
+++ Data ANALisys/2022_09_30(2).py
@@ -6,3 +6,10 @@ if a<9:
     print("La variabile è minore di 9")
 else:
     print("La variabile non è minore di 9")
+A= True
+B= False
+
+if A and B : 
+    print("condizioni tutte e due vere")
+
+
---
+++ Data ANALisys/2022_09_30(3).py
@@ -0,0 +1,9 @@
+#while
+i=0
+while i<10:
+    print (i)
+    i+=1 
+
+L=[1,2,"ciao","buon appetito"]
+for elemento in L:
+    print("l'elemento è di tipo",type(elemento))
