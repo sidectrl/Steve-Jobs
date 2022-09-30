188551a3b94ca818dc2a4857ab0e89c196eab415
Sidectrl <limonellifrancesco@gmail.com>
2022-09-30 12:44:20 +0200

    c

--- Data ANALisys/2022_09_30(3).py
+++ Data ANALisys/2022_09_30(3).py
@@ -7,3 +7,21 @@ while i<10:
 L=[1,2,"ciao","buon appetito"]
 for elemento in L:
     print("l'elemento è di tipo",type(elemento))
+
+"""
+esercizio: stampare tutti i numeri multipli di 7
+da 1 a 100
+"""
+
+A=range(100)
+for numero in range(100):
+    if numero%7==0:
+        print(numero)
+
+"""
+esercizio: stampare tutti i numeri multipli di 7 e di 3
+da 1 a 100 dispari
+"""
+for numero in range(100):
+    if numero%7==0 and numero%3==0 and (not numero%2==0):
+        print(numero)
