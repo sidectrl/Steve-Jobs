29ca6114e23753f12a2dad7fb90c91b80b7bdaae
Sidectrl <limonellifrancesco@gmail.com>
2022-10-06 20:13:18 +0200

    2022_10_06 esercizi per casa

---
+++ Data ANALisys/esercizi_per_casa/2022_10_06(Homework).py
@@ -0,0 +1,32 @@
+from itertools import count
+import random
+
+# counter[0] = numero 1, counter[1] = numero 2, counter[2] = numero 3,
+counter = [0, 0, 0, 0, 0, 0]
+# counter[3] = numero 4, counter[4] = numero 5, counter[5] = numero 6.
+
+
+for number in range(1000000):
+    nut = random.randint(1, 6)
+    if nut == 1:
+        counter[0] += 1
+    elif nut == 2:
+        counter[1] += 1
+    elif nut == 3:
+        counter[2] += 1
+    elif nut == 4:
+        counter[3] += 1
+    elif nut == 5:
+        counter[4] += 1
+    elif nut == 6:
+        counter[5] += 1
+
+
+for counting in range(len(counter)):
+    print("numero", counting+1, counter[counting])
+maximum = max(counter)
+print("Il numero lanciato più  frequentemente è:", counter.index(maximum)+1, "uscito", maximum, "volte")
+minor = min(counter)
+print("Il numero lanciato meno frequentemente è:", counter.index(minor)+1, "uscito", minor, "volte")
+
+
