54b1f2a0622241970f2038e78b48ceafb4299f45
Sidectrl <limonellifrancesco@gmail.com>
2022-11-08 18:33:24 +0100

    inserimento pisello come schermata

--- EtnaGames/tile/TileManager.java
+++ EtnaGames/tile/TileManager.java
@@ -28,7 +28,97 @@ public class TileManager {
     }
     public void draw(Graphics2D g2){
         g2.drawImage(tile[0].image, 0, 0, gp.getTileSize(), gp.getTileSize(), null);
-        g2.drawImage(tile[1].image, 1, 0, gp.getTileSize(), gp.getTileSize(), null);
-        g2.drawImage(tile[2].image, 1, 0, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 0, 48, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 0, 96, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 0, 144, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[0].image, 48, 0, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 96, 0, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 144, 0, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[0].image, 48, 48, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 96, 48, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 144, 48, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[0].image, 48, 96, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 96, 96, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 144, 96, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[0].image, 48, 144, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 96, 144, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 144, 144, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[0].image, 0, 240, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 0, 288, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 0, 336, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 0, 384, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[0].image, 48, 240, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 48, 288, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 48, 336, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 48, 384, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[0].image, 96, 240, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 96, 288, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 96, 336, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 96, 384, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[0].image, 144, 240, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 144, 288, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 144, 336, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 144, 384, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[1].image, 192, 144, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 192, 192, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 192, 240, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[1].image, 240, 144, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 240, 192, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 240, 240, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[1].image, 288, 144, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 288, 192, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 288, 240, gp.getTileSize(), gp.getTileSize(), null);
+        
+        g2.drawImage(tile[1].image, 336, 144, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 336, 192, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 336, 240, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[1].image, 384, 144, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 384, 192, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 384, 240, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[1].image, 432, 144, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 432, 192, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 432, 240, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[1].image, 480, 144, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 480, 192, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 480, 240, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[1].image, 528, 96, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 528, 144, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 528, 192, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 528, 240, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 528, 288, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[1].image, 576, 96, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 576, 144, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 576, 192, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 576, 240, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 576, 288, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[1].image, 624, 96, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 624, 144, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 624, 192, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 624, 240, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 624, 288, gp.getTileSize(), gp.getTileSize(), null);
+
+        g2.drawImage(tile[1].image, 672, 144, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[0].image, 672, 192, gp.getTileSize(), gp.getTileSize(), null);
+        g2.drawImage(tile[1].image, 672, 240, gp.getTileSize(), gp.getTileSize(), null);
+
+        //g2.drawImage(tile[1].image, 48, 0, gp.getTileSize(), gp.getTileSize(), null);
+        //g2.drawImage(tile[2].image, 96, 0, gp.getTileSize(), gp.getTileSize(), null);
     }
 }
