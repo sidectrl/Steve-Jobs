7265140c87315542e948abac4f83c0084e2ef67d
Sidectrl <limonellifrancesco@gmail.com>
2022-12-04 16:28:13 +0100

    esercitazione compito

---
+++ Web-Architecture/NPMSamples/FourthNPM/index.js
@@ -0,0 +1,22 @@
+let deck = [];
+let cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+let seeds = ["♤", "♧", "♡", "♢"];
+function initialize() {
+    //si deve utilizzare il metodo foreach della classe array per inizializzare il deck
+    
+    
+}
+
+exports.shuffle = () => {
+    return "";
+}
+
+exports.hasNext = () => {
+    return "";
+}
+
+exports.draw = () => {
+}
+
+initialize();
+console.log(deck);
