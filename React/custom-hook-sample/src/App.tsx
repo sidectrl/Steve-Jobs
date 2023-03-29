4ba76fcd1c45e79bbd74a6d41dd9e1e586af3a81
Sidectrl <limonellifrancesco@gmail.com>
2023-03-29 10:11:37 +0200

    Add( rick and morty character react app with custom hooks)

---
+++ React/custom-hook-sample/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Characters } from './components/Characters/Characters';
+
+function App() {
+  return (
+    <div>
+      <Characters/>
+    </div>
+  );
+}
+
+export default App;
