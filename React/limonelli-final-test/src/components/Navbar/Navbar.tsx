bb338068c8aae73cf532403033a12441e6381a9c
Sidectrl <limonellifrancesco@gmail.com>
2023-04-26 10:18:55 +0200

    feat(navbar): add navbar logo

--- React/limonelli-final-test/src/components/Navbar/Navbar.tsx
+++ React/limonelli-final-test/src/components/Navbar/Navbar.tsx
@@ -2,13 +2,19 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { TabContainer } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.png';
 
 function CustomNavbar() {
   return (
     <>
       <Navbar bg="dark" variant="dark" fixed='top'>
         <TabContainer>
-          <Navbar.Brand href="../../../logo.svg">Navbar</Navbar.Brand>
+        <img
+        style={{width: '100px'}}
+              src={logo}
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/classes">Classes</Nav.Link>
