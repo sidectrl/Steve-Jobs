1c97f24bccf4895205e4652dd8b84adcf1a6541c
Sidectrl <limonellifrancesco@gmail.com>
2023-05-03 00:27:39 +0200

    add(FavoriteCard): add favorite card file

---
+++ React-Native/React-Native-Limonelli-Francesco/src/components/Card/FavoriteCard.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { TouchableOpacity, View, Image, StyleSheet, Text } from "react-native";
+import { Result } from "../../models/Data";
+import { HeartButton } from "../HeartButton/HeartButton";
+
+interface Props {
+  item: Result;
+  index: number;
+  disabled?: boolean;
+  onPress?: () => void;
+}
+const FavoriteCard = ({ item, index, disabled, onPress }: Props) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.5}
+      key={`key-${index}`}
+      onPress={onPress}
+      disabled={disabled && !onPress}
+    >
+      <View style={styles.cardContainer}>
+        <Image
+          source={{
+            uri: item?.picture?.large,
+          }}
+          style={styles.image}
+        />
+        <Text>Cell:{item?.cell} </Text>
+        <HeartButton 
+        item={item}
+        />
+        <View style={styles.card}>
+          
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#fff",
+    height: 170,
+    width: "100%",
+    padding: 15,
+    borderRadius: 30,
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  card: {
+    flexDirection: "row",
+  },
+  image: {
+    height: 100,
+    width: 100,
+    borderRadius: 80,
+  },
+  description: {
+    fontWeight: "bold",
+    padding: 4,
+  },
+  tail: {
+    padding: 4,
+    width: 120,
+  },
+});
+
+export default FavoriteCard;
